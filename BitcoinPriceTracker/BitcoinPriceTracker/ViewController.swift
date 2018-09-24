156a26b7ca5ecf5d953f18c774459ccbbd1cc349
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2018-09-23 23:20:59 -0700

    initial commit started xcode project single application

---
+++ BitcoinPriceTracker/BitcoinPriceTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BitcoinPriceTracker
+//
+//  Created by Jonathan Cochran on 9/23/18.
+//  Copyright © 2018 Jonathan Cochran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
